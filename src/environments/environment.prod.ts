4727e029a164f684086e57d7f0bd643bc17ef089
ibrahim87-coder <I.Zakaria@st.ul.edu.lb>
2021-03-09 15:07:33 +0200

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCTFl5eydNGmai9-NUD_RncBnj2I-zNPaw",
+    authDomain: "kanban-fire-f0049.firebaseapp.com",
+    projectId: "kanban-fire-f0049",
+    storageBucket: "kanban-fire-f0049.appspot.com",
+    messagingSenderId: "928246340576",
+    appId: "1:928246340576:web:99db2498bc9931553e6985",
+    measurementId: "G-J0YB65VQKY"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCTFl5eydNGmai9-NUD_RncBnj2I-zNPaw",
+    authDomain: "kanban-fire-f0049.firebaseapp.com",
+    projectId: "kanban-fire-f0049",
+    storageBucket: "kanban-fire-f0049.appspot.com",
+    messagingSenderId: "928246340576",
+    appId: "1:928246340576:web:99db2498bc9931553e6985",
+    measurementId: "G-J0YB65VQKY"
+  }
+
 };
 
 /*
